f8644db77487ba54a7b7f14d6fd633ed3bc96ce8
baird <569147789@qq.com>
2017-03-02 14:58:23 +0800

    调测

--- hlh-project-parent/hlh-member/src/main/java/com/yeapoo/statistics/controller/base/BaseParam.java
+++
@@ -1,8 +0,0 @@
-package com.yeapoo.statistics.controller.base;
-
-/**
- *
- * Created by hebo on 2016/8/3.
- */
-public interface BaseParam {
-}
--- hlh-project-parent/hlh-member/src/test/java/service/BaseTest.java
+++ hlh-project-parent/hlh-member/src/test/java/service/BaseTest.java
@@ -7,8 +7,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({
-        "classpath:/spring/applicationContext-*.xml",
-        "classpath:/spring/springmvc-servlet.xml",
+        "classpath:/spring/joyous-member-applicationContext-base.xml",
 })
 public class BaseTest extends AbstractJUnit4SpringContextTests {
 }
